f5c27cfc92815efab7f6421322653db744cdd105
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2018-07-06 22:17:26 -0700

    Move app into flaskr directory Setup db.py with sqlite and essential CRUD functions Add initial schema Create endpoint to test inserting and reading from the database.

--- server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify, request
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-    username = request.args.get('username') or 'fakename'
-    test_response = { username: ['bar', 'bar2'] }
-    return jsonify(test_response)
